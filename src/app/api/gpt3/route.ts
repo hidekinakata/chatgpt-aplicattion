458e287577ccd43e1ee6e48e5efba9dcd2ed9d15
William Hideki Nakata <whnakata@gmail.com>
2023-04-13 23:16:24 -0300

    Update route.ts

--- src/app/api/gpt3/route.ts
+++ src/app/api/gpt3/route.ts
@@ -1,6 +1,10 @@
 import OAIStream, { Payload } from "@/lib/OpenIA";
 import { NextRequest } from "next/server";
 
+export const config = {
+  runtime: "edge",
+};
+
 interface ReqData extends Payload {}
 
 export async function POST(request: NextRequest) {
